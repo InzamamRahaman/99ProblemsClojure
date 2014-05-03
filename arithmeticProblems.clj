4ee6efc15f056db38a6af5654846b93a0e955558
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-05-02 23:22:30 -0400

    Refactored some code

--- arithmeticProblems.clj
+++ arithmeticProblems.clj
@@ -2,13 +2,16 @@
 ;; immediately with the results shown on the
 ;; right.
 
+(defn get-extra-factors [n]
+  (range 5 (inc (Math/ceil (Math/sqrt n))) 2))
+
 (defn is-prime [n]
   (cond
      (= n 1) false
      (or (= n 2) (= n 3)) true
      (= 0 (mod n 2)) false
      (= 0 (mod n 3)) false
-     (empty? (filter #(= 0 (mod n %1)) (range 5 (inc (Math/ceil (Math/sqrt n))) 2))) true
+     (empty? (filter #(= 0 (mod n %1)) (get-extra-factors n))) true
      :else false))
 
 (defn gcd [x y]
@@ -22,4 +25,17 @@
 
 (defn totient-naive [n]
   (count (filter (partial is-coprime n) (range 1 n))))
+
+
+;; Code to get greatest prime factor
+(defn remove-factor [n factor]
+  (loop [m n]
+    (if (= (mod m factor) 0)
+      (recur (/ m factor))
+      m)))
+
+(defn greatest-prime-factor [n]
+  (let [factors (conj (conj (get-extra-factors n) 3) 2)]
+    (reduce #(remove-factor %1 %2) n factors)))
+
 
